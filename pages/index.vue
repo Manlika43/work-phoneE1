6376d62e9dad78e832fed16f66a0beae6d06009c
Manlika43 <0833jung@gmail.com>
2020-07-08 15:45:26 +0700

    init1

--- pages/index.vue
+++ pages/index.vue
@@ -7,6 +7,11 @@
                  <v-btn rounded color="error" dark>ไปสู่เมนูหลัก</v-btn>
             </div>
              </nuxt-link>
+              <nuxt-link to="/Ex01">
+            <div class="text-center">
+                 <v-btn rounded color="error" dark>กลับหน้าล็อกอิน</v-btn>
+            </div>
+             </nuxt-link>
           </div>
   </div>
 </template>
