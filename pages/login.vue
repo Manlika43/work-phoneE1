c7e4b16e0c0b53639ad489f08eb41d1e10f6cba5
Manlika43 <0833jung@gmail.com>
2020-07-10 13:51:07 +0700

    init1

---
+++ pages/login.vue
@@ -0,0 +1,33 @@
+<template>
+
+   <v-card>
+     <v-card-title>
+       <h1>Login</h1>
+     </v-card-title>
+     <v-card-text>
+       <v-form>
+         <v-text-field label="Username" 
+          prepend-icon="mdi-account-circle"
+         />
+       </v-form>
+       <v-form>
+         <v-text-field label="Password" prepend-icon="lock" append-icon="visibility_off"/>
+       </v-form>
+      
+  
+  <v-card-actions>
+       <nuxt-link to="/register">
+    <v-btn class="ma-2" tile color="green darken-4" dark>REGISTER</v-btn>
+    </nuxt-link>
+     <v-spacer></v-spacer>
+     
+     <v-btn class="ma-2" tile color="light-blue darken-3" dark>LOGIN</v-btn>
+    
+ </v-card-actions>
+
+       
+     </v-card-text>
+   </v-card>
+   
+ 
+</template>
