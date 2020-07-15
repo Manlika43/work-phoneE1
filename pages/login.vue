16b01699ef4ce38996eac8b1864529539985489b
Manlika43 <0833jung@gmail.com>
2020-07-15 15:32:11 +0700

    init1

--- pages/login.vue
+++ pages/login.vue
@@ -1,16 +1,17 @@
 <template>
-
-   <v-card>
-     <v-card-title>
-       <h1>Login</h1>
-     </v-card-title>
+<center>
+  <br>
+   <v-card width="500" color="#1E88E5">
+     <v-card-title-center>
+      
+      <h1>LOGIN</h1>
+     </v-card-title-center>
      <v-card-text>
        <v-form>
          <v-text-field label="Username" 
           prepend-icon="mdi-account-circle"
-         />
-       
-        <v-text-field 
+          />
+          <v-text-field 
             :type="showPassword ? 'text' : 'password'" 
             label="Password"
             prepend-icon="mdi-lock"
@@ -18,23 +19,16 @@
             @click:append="showPassword = !showPassword"
           />
        </v-form>
-      
-  
-  <v-card-actions>
-       <nuxt-link to="/register">
-    <v-btn class="ma-2" tile color="green darken-4" dark>REGISTER</v-btn>
-    </nuxt-link>
-     <v-spacer></v-spacer>
-     
-     <v-btn class="ma-2" tile color="light-blue darken-3" dark>LOGIN</v-btn>
-    
- </v-card-actions>
-
-       
      </v-card-text>
+     <v-card-actions>
+    <nuxt-link to="\register"><v-btn class="ma-2" tile color="#00695C" dark>REGISTER</v-btn></nuxt-link>
+    <v-spacer></v-spacer>
+   <v-btn class="ma-2" tile color="light-blue darken-3" dark>LOGIN</v-btn>
+    </v-btn>
+    </v-btn>
+  </v-card-actions>
    </v-card>
-   
- 
+   </center>
 </template>
 <script>
 export default {
@@ -46,3 +40,4 @@ export default {
   }
 }
 </script>
+
